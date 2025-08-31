a748096ecdff6c241e139fa418984971bd9d6015
Nachoo04 <ignacio_cardinale@hotmail.com>
2025-08-31 10:08:51 -0300

    Primer commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import TarjetasProp from "@/app-example/components/Tarjetas";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <TarjetasProp/>
+    </View>
+  );
+}
